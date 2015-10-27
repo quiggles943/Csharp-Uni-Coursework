17672f026707fcd501aa66ef7e21cdd8aed4f788
quiggles943 <40163180@live.napier.ac.uk>
2015-10-27 15:33:38 +0000

    Ability to read in menu from file added

---
+++ Assessment 2/Menu Program/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Menu_Program
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int filelength = 0;
+        public string[,] menu = new string[100,5];
+        string menupath = @"..\..\menu.txt";
+        string filepath;
+        public MainWindow()
+        {
+            InitializeComponent();
+            filepath = System.IO.Path.GetFullPath(menupath);
+            
+            readin();
+        }
+
+        private void readin()
+        {
+            using (StreamReader r = new StreamReader(filepath))
+            {
+                while (r.ReadLine() != null) { filelength++; }
+            }
+            int i = 1;
+            string[] file = System.IO.File.ReadAllLines(filepath);
+            int len = file.Length;
+            while (i < (len))
+            {
+                string[] column = file[i].Split('\t');
+                int j = 0;
+                while (j < (column.Length))
+                {
+                    string buffer = column[j];
+                    menu[i, j] = buffer;
+                    j++;
+                }
+                i++;
+            }
+            int k = 1;
+        }
+
+
+    }
+}
---
+++ Assessment 2/Menu Program/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Globalization;
+namespace Menu_Program
+{
+    //Created by Paul Quigley
+    //Class is a student with relevant properties to hold information for the class
+    //Last Modified: 20/10/2015
+    public class Menu
+    {
+        public Menu()
+        {
+
+        }
+    }
+}
