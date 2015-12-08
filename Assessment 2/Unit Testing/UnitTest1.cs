426728ecb4e94779951715b4d2711e1cd8994a46
quiggles943 <40163180@live.napier.ac.uk>
2015-12-08 10:22:47 +0000

    added server test

--- Assessment 2/Unit Testing/UnitTest1.cs
+++ Assessment 2/Unit Testing/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using Menu_Program;
 using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Unit_Testing
@@ -10,10 +11,17 @@ namespace Unit_Testing
         [TestMethod]
         public void ServerTest()
         {
+            string expected = "Michael";
+            List<Server> servers = new List<Server>();
             Server server1 = new Server("Paul", 2);
             Server server2 = new Server("Michael", 8);
 
+            servers.Add(server1);
+            servers.Add(server2);
 
+            string actual = servers.Find(x => x.ID == 8).name;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
